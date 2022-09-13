217fd5cf122b124c538a37d24722e1c10e9a2504
YalemAb <yalemsew2006@gmail.com>
2022-09-14 00:14:54 +0300

    README added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,15 @@
  */
 int main(void)
 {
-print_alphabet();
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 return (0);
 }
