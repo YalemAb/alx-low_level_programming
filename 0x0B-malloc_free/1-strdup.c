ed7205fc21f3ca40ffacf04345dc8eb626c0aaae
YalemAb <yalemsew2006@gmail.com>
2022-10-05 00:29:58 +0300

    0x0B. C - malloc, free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strdup - Copy a string in to a dyanmic memory
+ * @str: string to be copied
+ *
+ * Return: a pointer to the duplicated string, or NULL if str = NULL
+ * or if insufficient memory was availableclear
+ * 
+ */
+char *_strdup(char *str)
+{
+	char *ch;
+	unsigned int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		ch = malloc(strlen(str));
+		for (i = 0; i < (unsigned int)strlen(str); i++)
+		{
+			ch[i] = str[i];
+		}
+		if (ch == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			return (ch);
+		}
+	}
+	free(ch);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN
 #define MAIN "main.h"
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif
