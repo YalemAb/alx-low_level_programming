c6eff8b83e35eaeb825a72c6a6aa6b23ce4e4222
YalemAb <yalemsew2006@gmail.com>
2022-10-05 02:46:00 +0300

    0x0B. C - malloc, free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,64 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-#include <string.h>
+#include <stdlib.h>
+
 /**
- * str_concat - concanest two strings
- * @s1: string 1
- * @s2: string 2
+ * str_concat - Concatenates two strings.
+ * @s1: The string to be concatenated upon.
+ * @s2: The string to be concatenated to s1.
  *
- * Return: a pointer to the concatned string, or NULL if s1 = s2 = NULL
- * or in case of failure
+ * Return: If concatenation fails - NULL.
+ *         Otherwise - a pointer the newly-allocated space in memory
+ *                     containing the concatenated strings.
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *ch;
-	unsigned int i;
+	char *concat_str;
+	int index, concat_index = 0, len = 0;
 
-	if (s1 != NULL && s2 != NULL)
-	{
-		ch = malloc(strlen(s1) + strlen(s2) + 1);
-		for (i = 0; i < (unsigned int)strlen(s1); i++)
-		{
-			ch[i] = s1[i];
-		}
-		for (i = 0; i < (unsigned int)strlen(s2); i++)
-		{
-			ch[i + (unsigned int)strlen(s1)] = s2[i];
-		}
-	}
-	else if (s1 == NULL && s2 != NULL)
-	{
-		ch = malloc(strlen(s2) + 1);
-		if (ch == NULL)
-		{
-			return (NULL);
-		}
-		for (i = 0; i < (unsigned int)strlen(s2); i++)
-		{
-			ch[i] = s2[i];
-		}
-	}
-	else if (s1 != NULL && s2 == NULL)
-	{
-		ch = malloc(strlen(s1) + 1);
-		if (ch == NULL)
-		{
-			return (NULL);
-		}
-		for (i = 0; i < (unsigned int)strlen(s1); i++)
-		{
-			ch[i] = s1[i];
-		}
-	}
-	else if (s1 == NULL && s2 == NULL)
-	{
-		ch = malloc(sizeof(char));
-		if (ch == NULL)
-		{
-			return (NULL);
-		}
-		ch[0] = '\0';
-	}
-	return (ch);
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
+
+	concat_str = malloc(sizeof(char) * len);
+
+	if (concat_str == NULL)
+		return (NULL);
+
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
+
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
+
+	return (concat_str);
 }
