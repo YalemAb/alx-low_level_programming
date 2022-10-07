bb21e3e4104e32a57f458571db6ca3ae786e2e77
YalemAb <yalemsew2006@gmail.com>
2022-10-07 16:07:48 +0300

    More malloc, free

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * string_nconcat - concanest two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: a pointer to the concatned string,
+ *         s1 followed by the first n bytes of s2, and null terminated
+ *         if function fails returns NULL
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concatstr;
+	unsigned int i, j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (n >= strlen(s2))
+		n = strlen(s2);
+	concatstr = malloc(strlen(s1) + n + 1);
+	if (concatstr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < strlen(s1); i++)
+		{
+			concatstr[i] = s1[i];
+		}
+		for (j = 0; j < n; j++)
+		{
+			concatstr[i + j] = s2[j];
+		}
+		concatstr[i + j] = '\0';
+		return (concatstr);
+	}
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN
 #define MAIN "main.h"
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif
