f55529c96f52e69e054f89d6cce7f712cada20ce
YalemAb <yalemsew2006@gmail.com>
2022-10-10 22:45:08 +0300

    C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG "dog.h"
+/**
+ * dog - pet animal
+ * @name: "Poppy"
+ * @age: age in years
+ * @owner: Owner's name
+ *
+ * Description: Defines the basic info about dogs
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
