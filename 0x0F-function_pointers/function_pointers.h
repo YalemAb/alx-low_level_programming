e7c598faf6340629fa378bfca34839acd469f94b
YalemAb <yalemsew2006@gmail.com>
2022-10-13 01:50:35 +0300

    C - Function pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,7 +2,6 @@
 #define FUNC_PTR "function_pointers.h"
 #include <stddef.h>
 void print_name(char *name, void(*f)(char *));
-void(*f)(char *);
 void array_iterator(int *array, size_t size, void(*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
